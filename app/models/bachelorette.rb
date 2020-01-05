67845163c1d535113d813ee5ace3de3e62baffcd
Joshua O'bannon <jtobannon@gmail.com>
2020-01-05 15:49:21 -0700

    Update model methods to be shorter queries accomplishing the same thing

--- app/models/bachelorette.rb
+++ app/models/bachelorette.rb
@@ -2,11 +2,13 @@ class Bachelorette <ApplicationRecord
   has_many :contestants
   
   def average_age 
-    Contestant.select(:age).where(bachelorette_id: self.id).average(:age)
+    # Contestant.select(:age).where(bachelorette_id: self.id).average(:age)
+    contestants.average(:age)    
   end
 
   def contestant_hometowns
-    Contestant.select(:hometown).where(bachelorette_id: self.id).pluck(:hometown)
+    # Contestant.select(:hometown).where(bachelorette_id: self.id).pluck(:hometown)
+    contestants.pluck(:hometown).uniq
   end 
 
 end
